301e5bcbfe4045aa597ea9a8bd768ebbde0e9920
ALESBERESIK\beres <beresik.ales@gmail.com>
2022-09-26 21:01:01 +0200

    projectId

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   reporter: "cypress-mochawesome-reporter",
-
+  projectId: "5ta1sm",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
