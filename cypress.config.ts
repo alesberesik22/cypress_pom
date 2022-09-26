ff4908d49ade844a0b8c981a83dad70827e033fd
ALESBERESIK\beres <beresik.ales@gmail.com>
2022-09-26 20:23:19 +0200

    import reported

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  chromeWebSecurity: false,
-  projectId: "5ta1sm",
+  reporter: "cypress-mochawesome-reporter",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
